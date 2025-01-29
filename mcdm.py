84d3830a4236b8ec4864a1f310ca9290517d170c
Nicolas <el.guerrerovillalbanicolas@gmail.com>
2025-01-29 08:02:54 -0500

    A problem with documents and Documentos already debugged

--- mcdm.py
+++ mcdm.py
@@ -1,1 +1,32 @@
-print("hello world")
+# C:\Users\USUARIO\Documentos\U\Math>
+
+allFactors = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 ]
+myFactors = [ ]
+
+
+def end(x):
+    print(f"yeahhhhhh:{x}")
+def gcd(x):
+    for factor in allFactors:
+        reminder = x % factor
+        if reminder == 0:
+            div = int(x / factor)
+            myFactors.append(factor)
+            if div == 1:
+                print(myFactors)
+                return end(div)
+            else:
+                return gcd(div)
+# def firstDivition(x):
+#     for factor in allFactors:
+#         reminder = x % factor
+#         if reminder == 0:
+#             div = int(x / factor)
+#             myFactors.append(factor)
+#             if div == 1:
+#                 return end(div)
+#             else:
+#                  print("oh not")
+myNumber = gcd(21)
+
+
